1e852ac872b54b674d6409a5900e9c793d159763
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-03-12 14:31:34 +0800

    cpp learning

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int hello = 3;
+  bool boolean = false;
+  string myString = "hello world\n";
+  const int minutesPerHour = 60;
+
+  cout << hello << endl;
+  cout << boolean << endl;
+  cout << myString;
+  cout << minutesPerHour << " minutes";
+}
